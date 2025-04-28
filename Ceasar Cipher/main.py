5024a5ed44300a0489047a3a8d7db43453837cb6
Şevval Aydoğan <160162299+sevvlay@users.noreply.github.com>
2025-04-28 17:41:37 +0300

    Add files via upload

---
+++ Ceasar Cipher/main.py
@@ -0,0 +1,36 @@
+import art
+print(art.logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+
+    for letter in original_text:
+
+        if letter in alphabet:
+            new_position = (alphabet.index(letter) + shift_amount) % len(alphabet)
+            output_text += alphabet[new_position]
+        else:
+            output_text += letter
+
+    print(f"{encode_or_decode}d result: {output_text}")
+
+should_continue = True
+
+while should_continue:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise, type 'no'.").lower()
+
+    if restart == "no":
+        should_continue = False
+        print("Goodbye")
